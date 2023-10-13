e355b99b2e72ce4cead2688110dda818f845b738
ignacio-leon-m <ig.leon@duocuc.cl>
2023-10-13 19:27:25 -0300

    clase 13/10

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clase1310',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
